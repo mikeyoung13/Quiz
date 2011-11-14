ca0ff46b5a28bf1b185e2dd802bde0cddc5fdd0e
Michael Young <mikeyoung13@gmail.com>
2011-11-13 22:09:26 -0500

    made connections

--- Quiz/QuizAppDelegate.h
+++ Quiz/QuizAppDelegate.h
@@ -10,8 +10,21 @@
 
 @interface QuizAppDelegate : NSObject <UIApplicationDelegate>
 {
+    int currentQuestionIndex;
+    
+    // the model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    // the view objects
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+    
     IBOutlet UIWindow *window;
 }
 @property (retain, nonatomic) UIWindow *window;
+
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 
 @end
