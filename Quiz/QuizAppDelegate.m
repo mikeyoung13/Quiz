4780528fbcf9f0e7729fdfc3c07f7b7c04f8090e
Michael Young <mikeyoung13@gmail.com>
2011-11-13 22:34:56 -0500

    add implementation

--- Quiz/QuizAppDelegate.m
+++ Quiz/QuizAppDelegate.m
@@ -10,6 +10,46 @@
 
 @implementation QuizAppDelegate
 
+- (id) init {
+    // call the superclass init
+    self = [super init];
+    if (self) {
+        // create two arrays and make pointers to them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        // add questions and answers to the attays
+        [questions addObject:@"What is 7 + 7?"];
+        [answers addObject:@"14"];
+        
+        [questions addObject:@"What is the capital of Vermont?"];
+        [answers addObject:@"Montpelier"];
+        
+        [questions addObject:@"From what is cognac made?"];
+        [answers addObject:@"Grapes"];
+        
+    }
+    
+    // return address of new object
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender {
+    currentQuestionIndex++;
+    if (currentQuestionIndex == [questions count]) {
+        currentQuestionIndex = 0;
+    }
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    NSLog(@"displaying question: %@", question);
+    [questionField setText:question];
+    [answerField setText:@"???"];
+    
+}
+- (IBAction)showAnswer:(id)sender {
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    [answerField setText:answer];
+}
+
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
